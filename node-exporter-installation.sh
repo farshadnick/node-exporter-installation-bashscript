a05a28ebd5e4788ddc516676d8eca34c48eea937
Farshad Nick <88557305+farshadnick@users.noreply.github.com>
2021-09-23 00:34:59 +0330

    Create node-exporter-installation.sh

---
+++ node-exporter-installation.sh
@@ -0,0 +1,23 @@
+export RELEASE="1.2.2"
+wget https://github.com/prometheus/node_exporter/releases/download/v$RELEASE/node_exporter-$RELEASE.linux-386.tar.gz
+tar xzf node_exporter-$RELEASE.linux-386.tar.gz
+cd node_exporter-$RELEASE.linux-386/
+mv ./node_exporter /usr/local/bin/
+useradd --no-create-home --shell /bin/false prometheus
+ useradd --no-create-home --shell /bin/false node_exporter
+
+echo  -n "
+Description=Node Exporter
+After=network.target
+
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=multi-user.target
+
+systemctl daemon-reload
+systemctl enable node_exporter.service
